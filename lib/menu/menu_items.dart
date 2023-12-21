06e888e53d9298caa1384709a52af49123f6e22a
on-brulera <luishenryramirezmontalvo746@gmail.com>
2023-12-21 09:54:12 -0500

    Añadir opciones al AppBar

---
+++ lib/menu/menu_items.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MenuItems{
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItems({
+    required this.title, 
+    required this.subTitle, 
+    required this.link, 
+    required this.icon
+  });
+}
+
+const appMenuItems = <MenuItems>[
+  MenuItems(title: 'Botones',subTitle: 'Muchos Botones', link: '/buttnos', icon: Icons.smart_button),
+  MenuItems(title: 'Tarjetas',subTitle: 'Contenedor estilizado', link: '/card', icon: Icons.credit_card),
+];

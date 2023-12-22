67a9f87d652795a8a5a43bb4162db7bfe8bffd69
on-brulera <luishenryramirezmontalvo746@gmail.com>
2023-12-22 16:53:55 -0500

    Screens creados y barril actualizados

---
+++ lib/presentation/screens/ui_controls/ui_controlls_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UiControlsScreen extends StatelessWidget {
+  
+  static const name = 'iu_controlls_screen';
+  
+  const UiControlsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
